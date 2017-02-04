8fb77863ec9047c611b2845c55bc5c2ac58ab524
theProgrammer27 <adityam994@gmail.com>
2017-02-04 22:41:42 +0530

    Need to fix some bugs

---
+++ EmployeeDetails/src/Employee/EmployeeDetails.java
@@ -0,0 +1,45 @@
+package Employee;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class EmployeeDetails
+ */
+@WebServlet("/EmployeeDetails")
+public class EmployeeDetails extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("test/html");
+		PrintWriter out = response.getWriter();
+		final double TAX = 0.1;
+		String name = request.getParameter("name");
+		String age = request.getParameter("age");
+		String salary = request.getParameter("salary");
+		//String nation = request.getParameter("nation");
+		
+		int salaryamount = Integer.parseInt(salary);
+		int intage = Integer.parseInt(age);
+		
+		if(intage >= 30 && salaryamount < 50000) {
+			salaryamount = salaryamount+5000;
+		}
+		double tax = salaryamount*TAX;
+		double salaryAfterTax = salaryamount*(1-TAX);
+		
+		out.println("HI "+name+" your tax is "+tax+" and salary after"
+				+ "deducting tax is "+salaryAfterTax);
+	}
+
+}
