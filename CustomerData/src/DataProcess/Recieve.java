6b000502c966349caceb0447eb1590343a6636cb
theProgrammer27 <adityam994@gmail.com>
2017-02-05 10:48:43 +0530

    fixed customer

--- CustomerData/src/DataProcess/Recieve.java
+++ CustomerData/src/DataProcess/Recieve.java
@@ -8,6 +8,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -37,7 +38,7 @@ public class Recieve extends HttpServlet {
 		final int TOTALATRIBUTES = 9;
 		
 		//Valaditaion
-		if(id.toLowerCase().charAt(0) != 'c' || id.length() > 4){
+/*		if(id.toLowerCase().charAt(0) != 'c' || id.length() > 4){
 			
 			out.println("Invalid ID! Please try again");
 			
@@ -46,65 +47,83 @@ public class Recieve extends HttpServlet {
 		if(aoi.length<2) {
 			out.println("Please select al least two options");
 		}
-		
+		*/
+		if(Recieve.validate(id, aoi)){
 		//writing
-		String input = null;
-		try {
-		bw = new BufferedWriter(new FileWriter("D:\\data.txt", true));
-		input = (name+","+age+","+gender+","+city+","+address+","+pin+","+id+","+phone+",");
-		bw.write(input);
-	
-		if(aoi != null){
-			for(int i = 0; i < aoi.length ; i++){
-				String area = (aoi[i]+"_");
-				bw.write(area);
+			String input = null;
+			try {
+			bw = new BufferedWriter(new FileWriter("D:\\data.txt", true));
+			input = (name+","+age+","+gender+","+city+","+address+","+pin+","+id+","+phone+",");
+			bw.write(input);
+		
+			if(aoi != null){
+				for(int i = 0; i < aoi.length ; i++){
+					String area = (aoi[i]+"_");
+					bw.write(area);
+				}
 			}
-		}
-		bw.write(",");
-		}
-		finally{
-			bw.close();
-		}
-		
-		//reading 
-		String output = null;
-		BufferedReader br = null;
-		try {
-			br = new BufferedReader(new FileReader("D:\\data.txt"));
-			output = br.readLine();
-			
-		}
-		catch (FileNotFoundException e){
-			e.printStackTrace();
-		}
-		finally{
-			br.close();
-		}
-		
-		//analysis
-		int malecount = 0;
-		int femalecount = 0;
-		int travelcount =0;
-		int readingcount= 0;
-		int agecount = 0;
-		int phcount = 0;
-		String [] data = output.split(",");
-		int custcount = data.length/TOTALATRIBUTES;
-		for(int i = 0;i<custcount;i++){
-			if(data[2+(i*TOTALATRIBUTES)].equalsIgnoreCase("male"))malecount++;
-			else if (data[2*i].equalsIgnoreCase("female"))femalecount++;
-			
-			if(data[7+(i*TOTALATRIBUTES)]==null)phcount++;
-			
-			if(Integer.parseInt(data[1+(i*TOTALATRIBUTES)])<18)agecount++;
-			
-			String[] aoidata = data[8+(i*TOTALATRIBUTES)].split("_");
-			for(String a : aoidata){
-				if(a.equalsIgnoreCase("reading"))readingcount++;
-				if(a.equalsIgnoreCase("travelling"))travelcount++;
+			bw.write(",");
+			}
+			finally{
+				bw.close();
 			}
 			
+			//reading 
+			String output = null;
+			BufferedReader br = null;
+			try {
+				br = new BufferedReader(new FileReader("D:\\data.txt"));
+				output = br.readLine();
+				
+			}
+			catch (FileNotFoundException e){
+				e.printStackTrace();
+			}
+			finally{
+				br.close();
+			}
+			
+			//analysis
+			int malecount = 0;
+			int femalecount = 0;
+			int travelcount =0;
+			int readingcount= 0;
+			int agecount = 0;
+			int phcount = 0;
+			String [] data = output.split(",");
+			int custcount = data.length/TOTALATRIBUTES;
+			for(int i = 0;i<custcount;i++){
+				if(data[2+(i*TOTALATRIBUTES)].equalsIgnoreCase("male"))malecount++;
+				else if (data[2*i].equalsIgnoreCase("female"))femalecount++;
+				
+				if(data[7+(i*TOTALATRIBUTES)]==null)phcount++;
+				
+				if(Integer.parseInt(data[1+(i*TOTALATRIBUTES)])<18)agecount++;
+				
+				String[] aoidata = data[8+(i*TOTALATRIBUTES)].split("_");
+				for(String a : aoidata){
+					if(a.equalsIgnoreCase("reading"))readingcount++;
+					if(a.equalsIgnoreCase("travelling"))travelcount++;
+				}
+				
+			}
+			out.println(malecount+" "+femalecount+" "+travelcount+" "+readingcount+" "+agecount+" "+phcount);
 		}
-		out.println(malecount+" "+femalecount+" "+travelcount+" "+readingcount+" "+agecount+" "+phcount);
+		else{
+			RequestDispatcher rd = request.getRequestDispatcher("error.html");
+			rd.forward(request, response);
+		}
 	}
+
+
+static boolean validate(String id,String[] aoi){
+	if(id.toLowerCase().charAt(0) != 'c' || id.length() > 4){
+		return false;
+	}
+
+	if(aoi.length<2) {
+		return false;
+	}
+	return true;
+}
 }
